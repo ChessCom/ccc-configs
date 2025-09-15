1e7cb644e30f7eeb3638c7b3f2425c42db188d56
Andrew Grant <andrew.github@grantnet.us>
2025-09-15 11:11:57 -0400

    Updating download mechanism

--- scripts/download.py
+++ scripts/download.py
@@ -7,45 +7,39 @@ import shutil
 import tempfile
 import zipfile
 
-def download_logfile(tour_id, game_id, out_dir):
+def download_logfile(start_id, game_num, out_dir):
 
     working_dir   = os.path.dirname(os.path.abspath(__file__))
-    new_file_path = os.path.join(working_dir, out_dir, '%d-%d.log' % (tour_id, game_id))
+    new_file_path = os.path.join(working_dir, out_dir, '%d.log' % (start_id + game_num))
 
     if os.path.exists(new_file_path):
         return
 
-    print ('Downloading game #%d from %d' % (game_id - tour_id, tour_id))
-
-    base_url = 'https://cccfiles.chess.com/archive/cutechess.debug-%d-%d.zip'
-    response = requests.get(base_url % (tour_id, game_id))
+    base_url = 'https://storage.googleapis.com/chess-1-prod-ccc/gamelogs/game-%d.log'
+    response = requests.get(base_url % (start_id + game_num))
     assert response.status_code == 200
 
-    with tempfile.NamedTemporaryFile(delete=False) as temp_zip:
-        temp_zip.write(response.content)
+    with open(new_file_path, 'wb') as fout:
+        fout.write(response.content)
 
-    with tempfile.TemporaryDirectory() as temp_dir:
+    print ('Downloaded game #%d' % (game_num + 1))
 
-        with zipfile.ZipFile(temp_zip.name, 'r') as zip_ref:
-            zip_ref.extractall(temp_dir)
-
-        old_file_path = os.path.join(temp_dir, os.listdir(temp_dir)[0])
-        shutil.copy(old_file_path, new_file_path)
-
-def download_event(tour_id, games, out_dir, start=0):
-    for f in range(start, games):
-        download_logfile(tour_id, tour_id + 1 + f, out_dir)
-
+def download_event(start_id, games, out_dir, start=0):
+    for game_num in range(games):
+        try:
+            download_logfile(start_id, game_num, out_dir)
+        except Exception as error:
+            print ('Failed to download game #%d' % (game_num + 1))
 
 if __name__ == '__main__':
 
     p = argparse.ArgumentParser()
-    p.add_argument('--tour',  type=int, required=True)
-    p.add_argument('--games', type=int, required=True)
-    p.add_argument('--out',   type=str, default='data')
+    p.add_argument('--start',  type=int, required=True)
+    p.add_argument('--games',  type=int, required=True)
+    p.add_argument('--out',    type=str, default='data')
     args = p.parse_args()
 
     if not os.path.exists(args.out):
         os.makedirs(args.out)
 
-    download_event(args.tour, args.games, args.out)
+    download_event(args.start, args.games, args.out)
---
+++ scripts/events.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+EVENTS = {
+    'CCC v2 Testing Pt. 12' : { 'start' : 161599, 'games' : 2380 },
+}
