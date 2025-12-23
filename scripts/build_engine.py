a85785083a400a9102dd2f11ea41a534e15b9643
Andrew Grant <andrew.github@grantnet.us>
2025-12-23 00:04:08 -0500

    Update from old CCC build_engine.py

--- scripts/build_engine.py
+++ scripts/build_engine.py
@@ -7,21 +7,6 @@ import subprocess
 import sys
 import time
 
-NIDs = {
-    'stockfish'   : 134798, 'dragon'      : 184247, 'igel'        : 194311, 'minic'       : 194313,
-    'weiss'       : 194315, 'seer'        : 194319, 'berserk'     : 237525, 'revenge'     : 248822,
-    'arasan'      :    263, 'booot'       :    266, 'ethereal'    :    274, 'winter'      :  27577,
-    'rubi'        :  27766, 'wasp'        :    296, 'blackmarlin' : 298296, 'velvet'      : 298345,
-    'tucano'      : 298351, 'stash'       : 304831, 'smallbrain'  : 305443, 'marvin'      : 305445,
-    'uralochka'   : 305548, 'clover'      : 331673, 'caissa'      : 331675, 'torch'       : 345753,
-    'svart'       : 352619, 'whitecore'   : 352620, 'carp'        : 352621, 'viridithas'  : 352622,
-    'altair'      : 352623, 'stormphrax'  : 352624, 'stockdory'   : 352625, 'equisetum'   : 352627,
-    'obsidian'    : 366189, 'midnight'    : 372997, 'willow'      : 372999, 'akimbo'      : 373001,
-    'plentychess' : 376027, 'minitorch'   : 395059, 'minifish'    : 395060, 'halogen'     : 194321,
-    'patricia'    : 396677, 'integral'    : 399594, 'clarity'     : 399596, 'renegade'    : 399598,
-    'rofchade'    :  18526, 'heimdall'    : 422506, 'reckless'    : 422701, 'koivisto'    : 194317,
-}
-
 def gather_secrets():
     return [f for f in os.listdir('../secrets') if not f.startswith('.')]
 
@@ -34,9 +19,6 @@ def build_command(args, engine):
         base_command = 'DOCKER_BUILDKIT=1 sudo docker build'
     else:
         base_command = 'DOCKER_BUILDKIT=1 docker build'
-
-    if engine == 'lc0':
-        base_command = base_command.replace('docker', 'nvidia-docker')
 
     if args.verbose:
         base_command += ' --progress plain'
@@ -54,9 +36,7 @@ def build_command(args, engine):
 
 def get_version(args, engine):
 
-    runtime = 'nvidia-docker' if engine == 'lc0' else 'docker'
-
-    cmd = ['%s run --rm -i ccc-engines/%s' % (runtime, engine)]
+    cmd = ['docker run --rm -i ccc-engines/%s' % (engine)]
 
     if args.sudo:
         cmd[0] = 'sudo ' + cmd[0]
@@ -75,24 +55,6 @@ def get_version(args, engine):
 
     return ' '.join(line.split()[3:])
 
-def update_version(args, engine):
-
-    if not os.path.exists('../secrets/.update_endpoint'):
-        raise Exception('No known server update endpoint')
-
-    params = {
-        'id'      : NIDs[engine],
-        'version' : get_version(args, engine),
-    }
-
-    with open('../secrets/.update_endpoint') as fin:
-        url = fin.readline().rstrip()
-
-    for ii in range(5):
-        requests.post(url, data=params)
-
-    print ('Updated version for %s to %s' % (engine, params['version']))
-
 if __name__ == '__main__':
 
     # Always working relative to this script
@@ -102,7 +64,6 @@ if __name__ == '__main__':
     p.add_argument('engine',    help='Engine Name')
     p.add_argument('--dry',     help='Print build command only'       , action='store_true')
     p.add_argument('--skip',    help='Skip building entirely'         , action='store_true')
-    p.add_argument('--update',  help='Update the CCC server version'  , action='store_true')
     p.add_argument('--sudo',    help='Run docker commands with sudo'  , action='store_true')
     p.add_argument('--verbose', help='Use plain progress Docker style', action='store_true')
     args = p.parse_args()
@@ -114,6 +75,3 @@ if __name__ == '__main__':
     if not args.skip:
         os.system(build_command(args, args.engine))
         print ('Built version %s for %s' % (get_version(args, args.engine), args.engine))
-
-    if args.update:
-        update_version(args, args.engine)
